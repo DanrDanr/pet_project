c3a321071bf4c590348c6292736dc78c4054decd
danr <2286684456@qq.com>
2023-10-30 15:02:35 +0800

    你好张丹

--- src/main/java/org/pet/home/controller/EmployController.java
+++ src/main/java/org/pet/home/controller/EmployController.java
@@ -1,6 +1,10 @@
 package org.pet.home.controller;
 
 /**
+ * 红色：还没有add
+ * 绿色: add到git的缓存区，但是还没有commit(提交)到本地仓库
+ * 浅蓝色: 说明在本地的仓库，但是还没有push到远程仓库
+ * git commit
  *
  * @description:
  * @author: 22866
