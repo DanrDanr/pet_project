9d58e67b237cf9fe018dbe21d615fa059217df25
danr <2286684456@qq.com>
2023-10-31 10:32:10 +0800

    修改工具

--- src/main/java/org/pet/home/controller/HelloDanrController.java
+++
@@ -1,9 +0,0 @@
-package org.pet.home.controller;
-
-/**
- * @description:
- * @author: 22866
- * @date: 2023/10/30
- **/
-public class HelloDanrController {
-}
--- src/main/java/org/pet/home/utils/DepartmentParam.java
+++ src/main/java/org/pet/home/utils/DepartmentParam.java
@@ -1,6 +1,5 @@
 package org.pet.home.utils;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -8,6 +7,7 @@ import lombok.Data;
  * @author: 22866
  * @date: 2023/10/29
  **/
+@Data
 public class DepartmentParam {
     private String sn;
     private String name;
@@ -15,65 +15,4 @@ public class DepartmentParam {
     private  int state;
     private long parentId;
 
-    public DepartmentParam() {
-    }
-
-    public DepartmentParam(String sn, String name, String dirPath, int state, long parentId) {
-        this.sn = sn;
-        this.name = name;
-        this.dirPath = dirPath;
-        this.state = state;
-        this.parentId = parentId;
-    }
-
-    public String getSn() {
-        return sn;
-    }
-
-    public void setSn(String sn) {
-        this.sn = sn;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDirPath() {
-        return dirPath;
-    }
-
-    public void setDirPath(String dirPath) {
-        this.dirPath = dirPath;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-    public void setState(int state) {
-        this.state = state;
-    }
-
-    public long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(long parentId) {
-        this.parentId = parentId;
-    }
-
-    @Override
-    public String toString() {
-        return "DepartmentParam{" +
-                "sn='" + sn + '\'' +
-                ", name='" + name + '\'' +
-                ", dirPath='" + dirPath + '\'' +
-                ", state=" + state +
-                ", parentId=" + parentId +
-                '}';
-    }
 }
